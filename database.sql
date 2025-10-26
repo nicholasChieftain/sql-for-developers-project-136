a3add622ccfedbf7c1704de92a69d9c9acd5573f
Nicholas Chieftain <nmmalts@gmail.com>
2025-10-26 21:07:41 +0500

    Добавил таблицы: пользователей, ролей и связок для групп-пользователей

--- database.sql
+++ database.sql
@@ -60,3 +60,32 @@ create table program_modules
     program_id bigint references programs (id),
     module_id bigint references modules (id)
 );
+
+create domain email_address as text check (VALUE ~* '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$');
+
+create table teaching_groups
+(
+    id bigint primary key generated always as identity,
+    slug varchar(255),
+    created_at timestamp,
+    updated_at timestamp
+);
+
+create table user_roles 
+(
+    id bigint primary key generated always as identity,
+    name varchar(255)
+);
+
+create table users 
+(
+    id bigint primary key generated always as identity,
+    username varchar(255),
+    email email_address,
+    password text,
+    teaching_group_id bigint references teaching_groups (id),
+    role_id bigint references user_roles (id),
+    created_at timestamp,
+    updated_at timestamp
+);
+
